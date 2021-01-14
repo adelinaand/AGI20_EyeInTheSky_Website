0e5edc31696c15efd0224eb63ed5d34c6aae6d40
porsbjer <porsbjer@kth.se>
2021-01-14 11:16:10 +0200

    smal fix to imageGallery

--- imageGallery.js
+++ imageGallery.js
@@ -2,14 +2,17 @@ $(document).ready(function () {
     
     var modal = document.getElementById("ig-modal");
     var modalImg = document.getElementById("modal-img");
-    //var modal = $("#ig-modal")
+
     $(".ig-img").click(function(event) {
         modalImg.src = $(event.target).attr('src');
         modal.style.display = "block";
-        //modalImg.src = this.src;
     });
     
     var span = document.getElementsByClassName("close")[0];
+
+    function nextImg(n){
+
+    }
 
     span.onclick = function() {
         modal.style.display = "none";
